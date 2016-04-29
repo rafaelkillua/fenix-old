911ed23152426c963393905daaf272085a0145e8
Rafael Sampaio Tavares <rafael.tavares@ccc.ufcg.edu.br>
2016-04-28 23:39:56 -0300

    Tabelas de entrada e saída funcionais, com edição e remoção; adição de entradas e saídas funcionando.

--- src/br/com/rafaelst/fenix/controllers/Controller.java
+++ src/br/com/rafaelst/fenix/controllers/Controller.java
@@ -3,24 +3,25 @@ package br.com.rafaelst.fenix.controllers;
 import br.com.rafaelst.fenix.controllers.util.CriadorAlerta;
 import br.com.rafaelst.fenix.models.Entrada;
 import br.com.rafaelst.fenix.models.Saida;
+import javafx.application.Platform;
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.TableColumn.CellEditEvent;
-import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.util.converter.DoubleStringConverter;
 
 import java.net.URL;
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -35,23 +36,21 @@ public class Controller implements Initializable {
     @FXML private Button botaoMinimizar;
 
     @FXML private TableView<Entrada> tabelaEntrada;
-    @FXML private TableColumn colunaDataEntrada;
-    @FXML private TableColumn colunaHistoricoEntrada;
-    @FXML private TableColumn colunaValor;
+    @FXML private TableView<Saida> tabelaSaida;
 
     @FXML private TextField campoTotalEntradas;
-
-    @FXML private TableView<Saida> tabelaSaida;
-    @FXML private TableColumn colunaDataSaida;
-    @FXML private TableColumn colunaHistoricoSaida;
-    @FXML private TableColumn colunaDepositos;
-    @FXML private TableColumn colunaContas;
 
     @FXML private TextField campoTotalContas;
     @FXML private TextField campoTotalDepositos;
     @FXML private TextField campoTotalSaidas;
 
     @FXML private TextField campoSaldo;
+
+    @FXML private TextField campoData;
+    @FXML private TextField campoHistorico;
+    @FXML private TextField campoEntradaValor;
+    @FXML private TextField campoSaidaConta;
+    @FXML private TextField campoSaidaDeposito;
 
     private ObservableList<Entrada> dadosEntrada = FXCollections.observableArrayList();
     private ObservableList<Saida> dadosSaida = FXCollections.observableArrayList();
@@ -60,23 +59,18 @@ public class Controller implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         ativaMovimentoJanela();
 
-        for (int i = 0; i < 5; i++) {
-            dadosEntrada.add(new Entrada(i, String.valueOf(i*30), "oi", i*50));
-            dadosSaida.add(new Saida(i, String.valueOf(i*50), "io", i*10, i*10));
-        }
         tabelaEntrada.setItems(dadosEntrada);
         tabelaSaida.setItems(dadosSaida);
 
-        ativaEdicaoTabelaEntrada();
-        ativaEdicaoTabelaSaida();
+        inicializarEnterListeners();
 
-        atualizarTotalEntradas();
+        atualizarTodosOsCampos();
 
-        atualizarTotalContas();
-        atualizarTotalDepositos();
-        atualizarTotalSaidas();
+        resetarCamposEntrada();
 
-        atualizarSaldo();
+        inicializarPopupTabelas();
+
+        Platform.runLater(() -> campoHistorico.requestFocus());
     }
 
     private void ativaMovimentoJanela() {
@@ -88,97 +82,6 @@ public class Controller implements Initializable {
             gridPane.getScene().getWindow().setX(event.getScreenX() - xOffset);
             gridPane.getScene().getWindow().setY(event.getScreenY() - yOffset);
         });
-    }
-
-    private void ativaEdicaoTabelaEntrada() {
-        colunaDataEntrada.setCellFactory(TextFieldTableCell.<Entrada>forTableColumn());
-        colunaDataEntrada.setOnEditCommit(
-                new EventHandler<CellEditEvent<Entrada, String>>() {
-                    @Override
-                    public void handle(CellEditEvent<Entrada, String> t) {
-                        (t.getTableView().getItems().get(
-                                t.getTablePosition().getRow())
-                        ).setData(t.getNewValue());
-                    }
-                }
-        );
-
-        colunaHistoricoEntrada.setCellFactory(TextFieldTableCell.<Entrada>forTableColumn());
-        colunaHistoricoEntrada.setOnEditCommit(
-                new EventHandler<CellEditEvent<Entrada, String>>() {
-                    @Override
-                    public void handle(CellEditEvent<Entrada, String> t) {
-                        (t.getTableView().getItems().get(
-                                t.getTablePosition().getRow())
-                        ).setHistorico(t.getNewValue());
-                    }
-                }
-        );
-
-        colunaValor.setCellFactory(TextFieldTableCell.<Entrada, Double>forTableColumn(new DoubleStringConverter()));
-        colunaValor.setOnEditCommit(
-                new EventHandler<CellEditEvent<Entrada, Double>>() {
-                    @Override
-                    public void handle(CellEditEvent<Entrada, Double> t) {
-                        (t.getTableView().getItems().get(
-                                t.getTablePosition().getRow())
-                        ).setValor(t.getNewValue());
-                        atualizarTotalEntradas();
-                    }
-                }
-        );
-    }
-
-    private void ativaEdicaoTabelaSaida() {
-        colunaDataSaida.setCellFactory(TextFieldTableCell.<Saida>forTableColumn());
-        colunaDataSaida.setOnEditCommit(
-                new EventHandler<CellEditEvent<Saida, String>>() {
-                    @Override
-                    public void handle(CellEditEvent<Saida, String> t) {
-                        (t.getTableView().getItems().get(
-                                t.getTablePosition().getRow())
-                        ).setData(t.getNewValue());
-                    }
-                }
-        );
-
-        colunaHistoricoSaida.setCellFactory(TextFieldTableCell.<Saida>forTableColumn());
-        colunaHistoricoSaida.setOnEditCommit(
-                new EventHandler<CellEditEvent<Saida, String>>() {
-                    @Override
-                    public void handle(CellEditEvent<Saida, String> t) {
-                        (t.getTableView().getItems().get(
-                                t.getTablePosition().getRow())
-                        ).setHistorico(t.getNewValue());
-                    }
-                }
-        );
-
-        colunaContas.setCellFactory(TextFieldTableCell.<Saida, Double>forTableColumn(new DoubleStringConverter()));
-        colunaContas.setOnEditCommit(
-                new EventHandler<CellEditEvent<Saida, Double>>() {
-                    @Override
-                    public void handle(CellEditEvent<Saida, Double> t) {
-                        (t.getTableView().getItems().get(
-                                t.getTablePosition().getRow())
-                        ).setContas(t.getNewValue());
-                        atualizarTotalEntradas();
-                    }
-                }
-        );
-
-        colunaDepositos.setCellFactory(TextFieldTableCell.<Saida, Double>forTableColumn(new DoubleStringConverter()));
-        colunaDepositos.setOnEditCommit(
-                new EventHandler<CellEditEvent<Saida, Double>>() {
-                    @Override
-                    public void handle(CellEditEvent<Saida, Double> t) {
-                        (t.getTableView().getItems().get(
-                                t.getTablePosition().getRow())
-                        ).setDepositos(t.getNewValue());
-                        atualizarTotalEntradas();
-                    }
-                }
-        );
     }
 
     @FXML
@@ -239,11 +142,116 @@ public class Controller implements Initializable {
         stage.setIconified(true);
     }
 
-    public void atualizarTotalEntradas() {
+    @FXML
+    protected void adicionar() {
+        if (!campoEntradaValor.getText().isEmpty()) {
+            try {
+                dadosEntrada.add(new Entrada(0, campoData.getText(), campoHistorico.getText(), Double.parseDouble(campoEntradaValor.getText())));
+            } catch (Exception e) {
+                CriadorAlerta.criarAlertaErro("Exceção", e.getClass() + e.getMessage());
+            }
+        }
+        if (!campoSaidaConta.getText().isEmpty() && !campoSaidaDeposito.getText().isEmpty()) {
+            try {
+                dadosSaida.add(new Saida(0, campoData.getText(), campoHistorico.getText(), Double.parseDouble(campoSaidaConta.getText()), Double.parseDouble(campoSaidaDeposito.getText())));
+            } catch (Exception e) {
+                CriadorAlerta.criarAlertaErro("Exceção", e.getClass() + e.getMessage());
+            }
+        } else if (!campoSaidaConta.getText().isEmpty()) {
+            try {
+                dadosSaida.add(new Saida(0, campoData.getText(), campoHistorico.getText(), Double.parseDouble(campoSaidaConta.getText()), 0));
+            } catch (Exception e) {
+                CriadorAlerta.criarAlertaErro("Exceção", e.getClass() + e.getMessage());
+            }
+        } else if (!campoSaidaDeposito.getText().isEmpty()) {
+            try {
+                dadosSaida.add(new Saida(0, campoData.getText(), campoHistorico.getText(), 0, Double.parseDouble(campoSaidaDeposito.getText())));
+            } catch (Exception e) {
+                CriadorAlerta.criarAlertaErro("Exceção", e.getClass() + e.getMessage());
+            }
+        }
+        atualizarTodosOsCampos();
+        resetarCamposEntrada();
+        puxarFocoHistorico();
+    }
+
+    private void inicializarEnterListeners(){
+        campoData.setOnKeyPressed((event) -> { if(event.getCode() == KeyCode.ENTER) { adicionar(); } });
+        campoHistorico.setOnKeyPressed((event) -> { if(event.getCode() == KeyCode.ENTER) { adicionar(); } });
+        campoEntradaValor.setOnKeyPressed((event) -> { if(event.getCode() == KeyCode.ENTER) { adicionar(); } });
+        campoSaidaConta.setOnKeyPressed((event) -> { if(event.getCode() == KeyCode.ENTER) { adicionar(); } });
+        campoSaidaDeposito.setOnKeyPressed((event) -> { if(event.getCode() == KeyCode.ENTER) { adicionar(); } });
+    }
+
+    private void inicializarPopupTabelas() {
+        tabelaEntrada.setRowFactory(tableView -> {
+            final TableRow<Entrada> row = new TableRow<>();
+            final ContextMenu contextMenu = new ContextMenu();
+            final MenuItem editarMenuItem = new MenuItem("Editar");
+            final MenuItem removerMenuItem = new MenuItem("Remover");
+            removerMenuItem.setOnAction(event -> {
+                Entrada temp = row.getItem();
+                tabelaEntrada.getItems().remove(temp);
+                apagarEntrada(temp);
+            });
+            editarMenuItem.setOnAction(event -> {
+                Entrada temp = row.getItem();
+                tabelaEntrada.getItems().remove(temp);
+                campoData.setText(temp.getData());
+                campoHistorico.setText(temp.getHistorico());
+                campoEntradaValor.setText(String.valueOf(temp.getValor()));
+                apagarEntrada(temp);
+            });
+            contextMenu.getItems().addAll(editarMenuItem, removerMenuItem);
+            row.contextMenuProperty().bind(
+                    Bindings.when(row.emptyProperty())
+                            .then((ContextMenu)null)
+                            .otherwise(contextMenu)
+            );
+            return row ;
+        });
+        tabelaSaida.setRowFactory(tableView -> {
+            final TableRow<Saida> row = new TableRow<>();
+            final ContextMenu contextMenu = new ContextMenu();
+            final MenuItem editarMenuItem = new MenuItem("Editar");
+            final MenuItem removerMenuItem = new MenuItem("Remover");
+            removerMenuItem.setOnAction(event -> {
+                Saida temp = row.getItem();
+                tabelaSaida.getItems().remove(temp);
+                apagarSaida(temp);
+            });
+            editarMenuItem.setOnAction(event -> {
+                Saida temp = row.getItem();
+                tabelaSaida.getItems().remove(temp);
+                campoData.setText(temp.getData());
+                campoHistorico.setText(temp.getHistorico());
+                campoSaidaConta.setText(String.valueOf(temp.getContas()));
+                campoSaidaDeposito.setText(String.valueOf(temp.getDepositos()));
+                apagarSaida(temp);
+            });
+            contextMenu.getItems().addAll(editarMenuItem, removerMenuItem);
+            row.contextMenuProperty().bind(
+                    Bindings.when(row.emptyProperty())
+                            .then((ContextMenu)null)
+                            .otherwise(contextMenu)
+            );
+            return row ;
+        });
+    }
+
+    private void apagarEntrada(Entrada entrada) {
+        dadosEntrada.remove(entrada);
+    }
+
+    private void apagarSaida (Saida saida) {
+        dadosSaida.remove(saida);
+    }
+
+    private void atualizarTotalEntradas() {
         campoTotalEntradas.setText(String.valueOf(getTotalEntradas()));
     }
 
-    public double getTotalEntradas() {
+    private double getTotalEntradas() {
         double total = 0;
         for (Entrada entrada : tabelaEntrada.getItems()) {
             total += entrada.getValor();
@@ -251,11 +259,11 @@ public class Controller implements Initializable {
         return total;
     }
 
-    public void atualizarTotalContas() {
+    private void atualizarTotalContas() {
         campoTotalContas.setText(String.valueOf(getTotalContas()));
     }
 
-    public double getTotalContas() {
+    private double getTotalContas() {
         double total = 0;
         for (Saida saida : tabelaSaida.getItems()) {
             total += saida.getContas();
@@ -263,11 +271,11 @@ public class Controller implements Initializable {
         return total;
     }
 
-    public void atualizarTotalDepositos() {
+    private void atualizarTotalDepositos() {
         campoTotalDepositos.setText(String.valueOf(getTotalDepositos()));
     }
 
-    public double getTotalDepositos() {
+    private double getTotalDepositos() {
         double total = 0;
         for (Saida saida : tabelaSaida.getItems()) {
             total += saida.getDepositos();
@@ -275,19 +283,46 @@ public class Controller implements Initializable {
         return total;
     }
 
-    public void atualizarTotalSaidas() {
+    private void atualizarTotalSaidas() {
         campoTotalSaidas.setText(String.valueOf(getTotalSaidas()));
     }
 
-    public double getTotalSaidas() {
+    private double getTotalSaidas() {
         return getTotalContas() + getTotalDepositos();
     }
 
-    public void atualizarSaldo() {
+    private void atualizarSaldo() {
         campoSaldo.setText(String.valueOf(getSaldo()));
     }
 
-    public double getSaldo() {
+    private double getSaldo() {
         return getTotalEntradas() - getTotalSaidas();
+    }
+
+    private void atualizarTodosOsCampos() {
+        atualizarTotalEntradas();
+        atualizarTotalContas();
+        atualizarTotalDepositos();
+        atualizarTotalSaidas();
+        atualizarSaldo();
+    }
+
+    private void resetarCamposEntrada() {
+        campoData.setText(getDataHoje());
+        campoHistorico.setText("");
+        campoEntradaValor.setText("");
+        campoSaidaConta.setText("");
+        campoSaidaDeposito.setText("");
+    }
+
+    private void puxarFocoHistorico() {
+        campoHistorico.requestFocus();
+    }
+
+    private String getDataHoje(){
+        final Calendar calendar = Calendar.getInstance();
+        int dia = calendar.get(Calendar.DAY_OF_MONTH);
+        String mes = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+        return dia + "/" + mes;
     }
 }
